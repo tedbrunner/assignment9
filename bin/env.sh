f7e25072891a9693a81325e4a027abf90e94be29
ted.brunner <ted.brunner@gmail.com>
2017-03-02 20:56:49 -0800

    first change

--- bin/env.sh
+++ bin/env.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
 # Setup Project Specfics - No Secrets
-export DOCKER_REPO=pdxdiver
+#export DOCKER_REPO=pdxdiver
+export DOCKER_REPO=tedbrunner
 export DOCKER_IMAGE=homeless-service
+#export DOCKER_IMAGE=django-web
 export PROJ_SETTINGS_DIR=homelessAPI
-export DEPLOY_TARGET=LOCAL
+#export DEPLOY_TARGET=LOCAL
+export DEPLOY_TARGET=INTEGRATION
+
 echo "##############################"
 echo  Your Local Project Environement
 echo "##############################"
--- bin/test-proj.sh
+++ bin/test-proj.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 source ./bin/env.sh
-docker-compose run homeless-service python manage.py test
+#docker-compose run homeless-service python manage.py test
+docker build -t homeless-service .
